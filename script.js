063220e5f4102378f2ba7bcbfa0d0f731965b28e
grant-and-olivia <granthughese@gmail.com>
2020-02-16 12:02:57 -0500

    token

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
 	$.ajax({
 		url: "https://api.github.com/repos/grant-and-olivia/my-dot-map/contents/users/grant.json",
 		headers: {
-			"Authorization": "token d4693e1609fdcee877007a638a25e4ff21b9ece2",
+			"Authorization": "token 6bc9758a2898033eacdd5583252fd4b148c94b63",
 			"Accept": "application/json"
 		},
 		dataType: "json",
